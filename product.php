9aef4e844aedd3f01394b3ec97a34a387f0b0f1c
anik4997 <44748778+anik4997@users.noreply.github.com>
2023-04-11 21:51:36 +0600

    Update product.php

--- product.php
+++ product.php
@@ -2,32 +2,34 @@
 include 'database.php';
 class product{
     public $db;
-
+    // This constractor is for creating an object for the class database where have all the connections(db connection, insert, show query connections)
     public function __construct(){
 
         $this->db = new database();
     }
     public function addProduct($data, $img){
+        // Taking values of input field by post and files(super global variable)
         $product_name = $data['product_name'];
         $product_details = $data['product_details'];
         $product_price = $data['product_price'];
         $vendor_email = $data['vendor_email'];
         $product_tax = $data['product_tax'];
 
-
+        // This variable is declared for specific types of files permission('jpg','jpeg','png','gif')
         $permited_img_type = array('jpg','jpeg','png','gif');
         $product_image = $img['product_image']['name'];
+        // This variable is declared for specific size of files permission less than 1mb
         $permited_img_size = $img['product_image']['size'];
         $img_tmp = $img['product_image']['tmp_name'];
 
         $div = explode('.',$product_image);
+        // This variable is declared for converting lowercase of the permitted files extendions ('jpg','jpeg','png','gif')
         $img_ext = strtolower(end($div));
         $unique_img_name = substr(md5(time()),0,10).'.'.$img_ext;
         $upload_img = "upload/".$unique_img_name;
 
 
-        // $rcv_id = $data['id'];
-
+        // These are the conditions of not empty fields, permitted file size and permitted file types
 
         if(empty($product_name) || empty($product_details) || empty($product_price) || empty($vendor_email) || empty($product_image) ){
             $empty_msg = "Field must not be empty!";
@@ -40,6 +42,7 @@ class product{
             return $extension_error_msg;
         }else{
             move_uploaded_file($img_tmp,$upload_img);
+            // Insert query
 
             $insert_query = "INSERT INTO oop_crud (product_name, product_details, product_price, vendor_email, product_image, tax) VALUES ('$product_name', '$product_details', '$product_price', '$vendor_email', '$upload_img', '$product_tax')";
 
@@ -55,23 +58,13 @@ class product{
             }
         } 
     }
+    //  This method is for select query for showing products
     public function show_products(){
         $show_query = "SELECT * FROM oop_crud";
         $select_query_connection = $this->db->select($show_query);
         return $select_query_connection;
     }
 
-    // public function show_cart(){
-    //     $cart_insert =  "INSERT INTO selected_products (id, product_name, product_price, vendor_email, product_image, tax, product_quantity)
-    //     SELECT id, product_name, product_price, vendor_email, product_image, tax, '$quantity'
-    //     FROM 	oop_crud
-    //     WHERE id = $rcv_id";
-
-
-    //     $cart_insert_connection = $this->db->cart_select($cart_insert);
-    //     return $cart_insert_connection;
-
-    // }
 }
 
 ?>
