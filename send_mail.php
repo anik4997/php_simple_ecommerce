a3a183e328cefbe1a70b30438f622baac2fb19d7
anik4997 <44748778+anik4997@users.noreply.github.com>
2023-04-12 21:42:02 +0600

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -1,12 +1,28 @@
 <?php
-include 'database_connection.php';
+include 'database.php';
+class mail{
+   public $db;
+   public function __construct(){
+        $this->db = new database();
+   }
+   public function send_mail(){
+    $mail_query = "SELECT * FROM selected_products";
+    $mail_query_connection = $this->db->send_mail_connection($mail_query);
+    return $mail_query_connection;
+   }
+   public function empty_cart(){
+    $empty_cart_query = "DELETE FROM selected_items";
+    $empty_cart_query_connection = $this->db->empty_cart($empty_cart_query);
+   }
+}
 if(isset($_POST['order_btn'])){
-    $mail_query = "SELECT * FROM selected_items";
-    $mail_query_connection = mysqli_query($connect,$mail_query);
-    
-    $count = mysqli_num_rows($mail_query_connection);
-if($count>0){
-    while($row = mysqli_fetch_assoc($mail_query_connection)){
+
+}
+$mail = new mail();
+$send_mail = $mail->send_mail();
+if ($send_mail){
+    // while looop for fetching data from database
+    while($row = mysqli_fetch_assoc($send_mail)){
         $db_mail = $row['vendor_email'];
         $subject = "You have an order!";
         $body = "Dear vendor, you have an order from our website plese check it out from your admin panel and complete the order asap.";
@@ -14,8 +30,7 @@ if($count>0){
         if(mail($db_mail,$subject,$body,$header)){
             echo "<h3>A notification email send successfully to <b>$db_mail</b> about your order....<br></h3> ";
             // Cart will be empty after placing the order
-            $empty_cart_query = "DELETE FROM selected_items";
-            $empty_cart_query_connection = mysqli_query($connect,$empty_cart_query);
+           $empty_cart = $mail->empty_cart();
            
         }else{
             echo "Email sending failed!";
@@ -23,6 +38,4 @@ if($count>0){
         }
     }
 }
-}
-
 ?>
