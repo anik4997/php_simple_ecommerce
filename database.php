1a45af892d5ca6eba18be214a37b1cb5b6f5774a
anik4997 <44748778+anik4997@users.noreply.github.com>
2023-04-12 15:28:37 +0600

    Update database.php

--- database.php
+++ database.php
@@ -13,7 +13,9 @@ class database{
     public function __construct(){
         $this->db_connect();
     }
-    // database connection
+
+
+    // Database connection
     public function db_connect(){
         $this->db_connection = mysqli_connect($this->db_host,$this->db_username,$this->db_password,$this->db_name);
         if(!$this->db_connection){
@@ -21,7 +23,9 @@ class database{
             return false;
         }
     }
-    // INSERT
+
+
+    // Insert products to database
     public function insert($insert_query){
         $insert_query_connection = mysqli_query($this->db_connection,$insert_query) or die($this->db_connection->error.__LINE__);
         if($insert_query_connection){
@@ -33,7 +37,7 @@ class database{
 
 
 
-    // SHOW
+    // Show products in available products section
     public function select($insert_query){
         $insert_query_connection = mysqli_query($this->db_connection,$insert_query) or die($this->db_connection->error.__LINE__);
         if(mysqli_num_rows($insert_query_connection)>0){
@@ -43,6 +47,27 @@ class database{
         }
     }
 
+
+    //Cart insert
+    public function insert_cart_table($cart_insert){
+        $cart_insert_connection = mysqli_query($this->db_connection,$cart_insert) or die($this->db_connection->error.__LINE__);
+        if($cart_insert_connection){
+            return $cart_insert_connection;
+        }else{
+            return false;
+        }
+    }
+
+    
+    // Cart show
+    public function show_cart($show_cart_query){
+        $show_cart_query_connection = mysqli_query($this->db_connection,$show_cart_query) or die($this->db_connection->error.__LINE__);
+        if(mysqli_num_rows($show_cart_query_connection)>0){
+            return $show_cart_query_connection;
+        }else{
+            return false;
+        }
+    }
 }
 
 ?>
