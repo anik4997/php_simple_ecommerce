86995fb86004f8681cc9d1b27b0c379465dd7795
anik4997 <44748778+anik4997@users.noreply.github.com>
2023-04-28 14:36:05 +0600

    Update database.php

--- database.php
+++ database.php
@@ -6,28 +6,39 @@ class database{
     public $db_username = USERNAME;
     public $db_password = PASSWORD;
     public $db_name = DATABASE;
-    public $db_connection;
-    public $db_error;
+    private static $instance = NULL;
+    private $db_conn;
 
 
-    public function __construct(){
-        $this->db_connect();
+    // Convert default constructor private
+    private function __construct(){}
+
+
+    // Creating a single instance for db connection
+    public static function getInstance(){
+        if(self::$instance == NULL){
+            self::$instance = new Static();
+        }
+        return self::$instance;
     }
 
 
     // Database connection
-    public function db_connect(){
-        $this->db_connection = mysqli_connect($this->db_host,$this->db_username,$this->db_password,$this->db_name);
-        if(!$this->db_connection){
-            die("Not connected".mysqli_error());
-            return false;
-        }
+    public static function db_connect(){
+            $db = self::getInstance();
+            $db->db_conn = mysqli_connect($db->db_host,$db->db_username,$db->db_password,$db->db_name);
+            return $db->db_conn;
+            if(!$db->db_conn){
+                die("Not connected".mysqli_error());
+                return false;
+            }
+        
     }
-
 
     // Insert products to database
     public function insert($insert_query){
-        $insert_query_connection = mysqli_query($this->db_connection,$insert_query) or die($this->db_connection->error.__LINE__);
+        $db = self::getInstance();
+        $insert_query_connection = mysqli_query($db->db_conn,$insert_query) or die($db->db_conn->error.__LINE__);
         if($insert_query_connection){
             return $insert_query_connection;
         }else{
@@ -39,7 +50,8 @@ class database{
 
     // Show products in available products section
     public function select($insert_query){
-        $insert_query_connection = mysqli_query($this->db_connection,$insert_query) or die($this->db_connection->error.__LINE__);
+        $db = self::getInstance();
+        $insert_query_connection = mysqli_query($db->db_conn,$insert_query) or die($db->db_conn->error.__LINE__);
         if(mysqli_num_rows($insert_query_connection)>0){
             return $insert_query_connection;
         }else{
@@ -50,7 +62,8 @@ class database{
 
     //Cart insert
     public function insert_cart_table($cart_insert){
-        $cart_insert_connection = mysqli_query($this->db_connection,$cart_insert) or die($this->db_connection->error.__LINE__);
+        $db = self::getInstance();
+        $cart_insert_connection = mysqli_query($db->db_conn,$cart_insert) or die($db->db_conn->error.__LINE__);
         if($cart_insert_connection){
             return $cart_insert_connection;
         }else{
@@ -61,7 +74,8 @@ class database{
 
     // Cart show
     public function show_cart($show_cart_query){
-        $show_cart_query_connection = mysqli_query($this->db_connection,$show_cart_query) or die($this->db_connection->error.__LINE__);
+        $db = self::getInstance();
+        $show_cart_query_connection = mysqli_query($db->db_conn,$show_cart_query) or die($db->db_conn->error.__LINE__);
         if(mysqli_num_rows($show_cart_query_connection)>0){
             return $show_cart_query_connection;
         }else{
@@ -71,16 +85,18 @@ class database{
 
     // Send notification mail to the vendor
     public function send_mail_connection($mail_query){
-        $mail_query_connection = mysqli_query($this->db_connection,$mail_query) or die($this->db_connection->error.__LINE__);
+        $db = self::getInstance();
+        $mail_query_connection = mysqli_query($db->db_conn,$mail_query) or die($db->db_conn->error.__LINE__);
         
     }
 
     // Empty cart
-    public function empty_cart_conncection($empty_cart_query){
-        $empty_cart_query_connection = mysqli_query($this->db_connection,$empty_cart_query) or die($this->db_connection->error.__LINE__);
+    public function empty_cart_connection($empty_cart_query){
+        $db = self::getInstance();
+        $empty_cart_query_connection = mysqli_query($db->db_conn,$empty_cart_query) or die($db->db_conn->error.__LINE__);
         
     }
 
 }
-
+$db_connect = database::db_connect();
 ?>
